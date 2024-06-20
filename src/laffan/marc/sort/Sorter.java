f87ac84be629a615d26534b7e841365d1344a0f5
Marc <marclaffan@gmail.com>
2024-06-20 22:48:42 +0100

    setup

---
+++ src/laffan/marc/sort/Sorter.java
@@ -0,0 +1,21 @@
+package laffan.marc.sort;
+
+import java.util.List;
+
+public class Sorter {
+
+	private static List data;
+	
+	
+	public Sorter()
+	{
+		
+	}
+	
+	public static void main(String args[]) {
+		Sorter sorter = new Sorter();
+		
+		
+		
+	}
+}
